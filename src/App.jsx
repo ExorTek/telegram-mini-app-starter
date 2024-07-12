5ee7f217882f30652f0748670b27a19cd6729f00
Memet <exortek979@gmail.com>
2024-07-12 03:50:46 +0300

    project structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WebApp from '@twa-dev/sdk'
-
+import '@styles/global.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    const element = document.createElement('div');
+    element.textContent = '+2';
+    element.style.position = 'absolute';
+    element.style.top = '30%';
+    element.style.left = '50%';
+    element.style.transform = 'translate(-50%, -50%)';
+    element.style.fontSize = '2rem';
+    element.style.color = 'red';
+    element.style.opacity = '1';
+    element.style.transition = 'all 0.6s';
+    element.style.cursor = 'default';
+    element.style.userSelect = 'none';
+    document.body.appendChild(element);
+    setTimeout(() => {
+      element.style.top = '0%';
+      element.style.opacity = '0';
+    }, 100);
+    setTimeout(() => {
+      element.remove();
+    }, 600);
+  };
 
   return (
-      <>
-          <div>
-              <a href="https://vitejs.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo"/>
-              </a>
-              <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo"/>
-              </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-              </button>
-              <p>
-                  Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
-          </div>
-          <div className="card">
-              <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-                  Show Alert
-              </button>
-          </div>
-      </>
-  )
+    <div className={'w-full max-w-[270px] h-full flex flex-col items-center justify-center mx-auto relative'}>
+      <img
+        className={'object-cover w-full h-full'}
+        src='/game-bg.jpg'
+        alt='game-bg'
+      />
+      <button
+        onClick={handleClick}
+        className={'absolute left-1/2 transform -translate-x-1/2 bottom-28 text-2xl text-red-500 font-bold w-full max-w-16 h-full max-h-16 rounded-full hover:bg-blue-300 hover:bg-opacity-20'}>
+        Click
+      </button>
+      <ul className={'absolute bottom-0 flex items-center justify-between w-full text-center'}>
+        <li className={'border py-6 w-full'}>Menu</li>
+        <li className={'border py-6 w-full'}>Menu</li>
+        <li className={'border py-6 w-full'}>Menu</li>
+        <li className={'border py-6 w-full'}>Menu</li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import WebApp from '@twa-dev/sdk'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import WebApp from '@twa-dev/sdk';
 
 WebApp.ready();
 
@@ -10,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
